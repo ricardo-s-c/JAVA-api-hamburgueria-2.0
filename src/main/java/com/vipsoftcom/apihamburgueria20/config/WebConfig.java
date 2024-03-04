3a1d7bf77471a65f5964fe4362ebfa0431bca8a6
rick010 <ricardo.dev.com@gmail.com>
2024-03-04 18:42:47 -0300

    add liberação de ports no secuty

--- src/main/java/com/vipsoftcom/apihamburgueria20/config/WebConfig.java
+++ src/main/java/com/vipsoftcom/apihamburgueria20/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-		.allowedOrigins("http://localhost:5173")
+		.allowedOrigins("http://localhost:5173","http://localhost:8080", "http://localhost:5432")
 		.allowedOrigins("*")
 		.allowedHeaders("*")
 		.allowedMethods("GET", "POST", "PUT", "DELETE") 
